1b5a96faec48a625d08accc343c7948b5cb69702
Melisaa1 <78279854+Melisaa1@users.noreply.github.com>
2021-02-21 18:06:32 +0300

    Add project files.

---
+++ DataAccess/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car {CarId=1, CategoryId=1, CarBrandId ="BMW", CarColorId = "siyah", CarModelYear= 1996, CarDailyPrice= 15000 },
+                new Car {CarId=2, CategoryId=1, CarBrandId ="Mercedes", CarColorId = "mavi", CarModelYear= 1998, CarDailyPrice= 12000 },
+                new Car {CarId=3, CategoryId=2,CarBrandId ="Megane", CarColorId = "yeşil", CarModelYear= 2000, CarDailyPrice= 10000 },
+                new Car {CarId=4, CategoryId=2, CarBrandId ="symbol", CarColorId = "sarı", CarModelYear= 2011, CarDailyPrice= 8000 },
+                new Car {CarId=5, CategoryId=2,CarBrandId ="clio", CarColorId = "pembe", CarModelYear= 2002, CarDailyPrice= 6000 }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        
+
+        public void Delete(Car car)
+        {
+            Car CarToDelete;
+            CarToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId); 
+        }
+
+        
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+       
+
+        public void UpDate(Car car)
+        {
+            Car carToUpDate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpDate.CarColorId = car.CarColorId;
+            carToUpDate.CarDailyPrice = car.CarDailyPrice;
+            carToUpDate.CarBrandId = car.CarBrandId;
+            carToUpDate.CarModelYear = car.CarModelYear;
+            carToUpDate.CategoryId = car.CategoryId;
+        }
+
+        List<Car> ICarDal.GetByld()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car: IEntity 
+    {
+        public int CarId { get; set; }
+        public int CategoryId { get; set; }
+        public string CarBrandId { get; set; }
+        public string CarColorId { get; set; }
+        public int CarModelYear { get; set; }
+        public int CarDailyPrice { get; set; }
+        public string CarDescription { get; set; }
+
+    }
+}
